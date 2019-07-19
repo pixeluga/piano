4149f82c2522ac47d033dbd891c36b004aa280cb
Taras Pixel <taraspixel@gmail.com>
2019-07-19 17:15:50 +0300

    add new class Fortepiano

--- piano.js
+++ piano.js
@@ -1,68 +1,73 @@
+const keysArrey = [
+    {id: 0, letter: 'C', keycode: '90', keyb: 'z', url: './sounds/C.mp3'},
+    {id: 1, letter: 'D', keycode: '88', keyb: 'x', url: './sounds/D.mp3'},
+    {id: 2, letter: 'E', keycode: '67', keyb: 'c', url: './sounds/E.mp3'},
+    {id: 3, letter: 'F', keycode: '86', keyb: 'v', url: './sounds/F.mp3'},
+    {id: 4, letter: 'G', keycode: '66', keyb: 'b', url: './sounds/G.mp3'},
+    {id: 5, letter: 'A', keycode: '78', keyb: 'n', url: './sounds/A.mp3'},
+    {id: 6, letter: 'B', keycode: '77', keyb: 'm', url: './sounds/B.mp3'},
+    {id: 7, letter: 'C2', keycode: '188', keyb: ',', url: './sounds/C2.mp3'}
+]
+
 class PianoKey {
-    constructor(character, keycode, keyb) {
-        this.character = character;
-        this.keycode = keycode;
-        this.keyb = keyb;
+    constructor(options) {
+        this.options = options;
+        this.pianokey = document.createElement('div');
     }
 
-    createKey(text = '-') {
-        const pianokey = document.createElement('div');
+    createKey() {
         const pianokeySpan = document.createElement('span');
         
-        pianokey.className = 'piano-key';
-        pianokey.id = this.character;
-        pianokey.setAttribute('data-key', this.keycode);
-        pianokey.addEventListener("click", this.onClick.bind(this));
-        pianokey.innerText = this.character;
+        this.pianokey.className = 'piano-key';
+        this.pianokey.id = this.options.letter;
+        this.pianokey.innerText = this.options.letter;
+        this.pianokey.setAttribute('data-key', this.options.keycode);
+        this.pianokey.addEventListener('click', this.onClick.bind(this));
+        
 
-        pianokeySpan.innerText = this.keyb;
+        pianokeySpan.innerText = this.options.keyb;
 
-        document.getElementById('root').append(pianokey);
-        pianokey.append(pianokeySpan);
+        document.getElementById('root').append(this.pianokey);
+        this.pianokey.append(pianokeySpan);
     }
 
-    onClick(event){
-        const pianokey = document.getElementById(this.character);
-        pianokey.className = 'piano-key tupped';
+    onClick() {
+        this.onPlay(this);    
+    }
 
-        onPlay(this.character);
+    onPlay(opt) {
+        if (opt.pianokey) {
+            opt.pianokey.classList.add('tupped');
+
+            setTimeout(() => {
+                opt.pianokey.classList.remove('tupped');
+            }, 500);
+        
+            const sound = new Audio(opt.options.url);
+            sound.play();
+        }
     }
 }
-const keysArrey = [
-    {id: 0, letter: 'C', keycode: '90', keyb: 'z'},
-    {id: 1, letter: 'D', keycode: '88', keyb: 'x'},
-    {id: 2, letter: 'E', keycode: '67', keyb: 'c'},
-    {id: 3, letter: 'F', keycode: '86', keyb: 'v'},
-    {id: 4, letter: 'G', keycode: '66', keyb: 'b'},
-    {id: 5, letter: 'A', keycode: '78', keyb: 'n'},
-    {id: 6, letter: 'B', keycode: '77', keyb: 'm'},
-    {id: 7, letter: 'C2', keycode: '188', keyb: ','}
-]
-function addKeys() {
-    keysArrey.forEach( 
-        (item) => new PianoKey(item.letter, item.keycode, item.keyb).createKey()
-    );
+
+class Fortepiano extends PianoKey{
+    constructor(arr) {
+        super();
+        this.arr = arr;
+    }
+    addKeys() {
+        this.arr.forEach( 
+            (item) => new PianoKey(item).createKey()
+        );
+    }
+    
 }
 
-addKeys();
-
-function onPlay(note) {
-    const pianokey = document.getElementById(
-        keysArrey.filter(item => item.letter === note)[0].letter
-    );
-
-    setTimeout(() => {
-        pianokey.className = 'piano-key';
-    }, 500);
-
-    const sound = new Audio(`./sounds/${note}.mp3`);
-    sound.play();
-}
+const pianino = new Fortepiano(keysArrey);
+pianino.addKeys();
 
 window.addEventListener('keydown', function(event) {
-    const keyCodeId = keysArrey.filter((x) => x.keycode == event.keyCode);
-    const pianokey = document.getElementById(keyCodeId[0].letter);
-    
-    pianokey.className = 'piano-key tupped';
-    onPlay(keyCodeId[0].letter);
+    const options = keysArrey.find((x) => x.keyb.toUpperCase() == event.key.toUpperCase());
+    const pianokey = options ? document.getElementById(options.letter) : null;
+   
+    pianino.onPlay({pianokey, options});
 })
